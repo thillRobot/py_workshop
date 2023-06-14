4198c34855f2d18933e83b56a60236c119cf1e06
thill <thill@tntech.edu>
2023-06-14 00:42:20 -0500

    updated module6_conditionals

--- modules/module6_conditionals/lecture6_conditionals/lecture6_conditionals.py
+++ modules/module6_conditionals/lecture6_conditionals/lecture6_conditionals.py
@@ -1,6 +1,6 @@
 # lecture6_conditionals
-# coefficients of quadratic equation
 
+# coefficients of quadratic equation
 coefs=input('Type a,b,c and press enter: ') # get input string from user
 a=float(coefs.split(',')[0])                # split items by commas 
 b=float(coefs.split(',')[1])                # access with index
@@ -8,6 +8,12 @@ c=float(coefs.split(',')[2])                # convert string to float
 
 x1=(-b+(b**2-4*a*c)**(0.5))/(2*a)           # calculate root values
 x2=(-b-(b**2-4*a*c)**(0.5))/(2*a)
-                         
+
 print('The first root is:', x1)
 print('The second root is:', x2)
+
+# case can be deterimined by descriminant
+if (b**2-4*a*c)>0:
+	print('The roots are real numbers')
+else:
+	print('The roots are complex numbers')
